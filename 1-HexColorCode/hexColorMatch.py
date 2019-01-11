854f942a9c1f6cd7963b5b992136deffcf0b66d2
Mark Manalo <mmanalo@dealersocket.com>
2019-01-11 09:30:07 -0800

    added sample color regex

---
+++ 1-HexColorCode/hexColorMatch.py
@@ -0,0 +1,43 @@
+import re
+
+# Hex code for a color
+txt = "#FF6600"
+
+def checkForMatchAndPrint(regexResult):
+  if (regexResult):
+    print("We have a match")
+    print(regexResult)
+  else:
+    print("No match")
+
+# the square brackets are used to group a range of acceptable characters
+# there are 3 basic quantifiers: 
+# ? - match zero or one time
+# * - match zero or more times
+# + - match one or more times
+x = re.findall("#[ABCDEF0123456789]+", txt)
+checkForMatchAndPrint(x)
+
+# for sequential characters we can group them with the '-' character
+x = re.findall("#[A-F0-9]+", txt)
+checkForMatchAndPrint(x)
+
+# adding a question mark after the # makes it optional (match zero or one time)
+# but this means it will match non hex codes like FF6
+x = re.findall("#?[A-F0-9]+", "FF6")
+checkForMatchAndPrint(x)
+
+# curly braces can add specificity, here we will match exactly 6 characters
+x = re.findall("#?[A-F0-9]{6}", "FF66")
+checkForMatchAndPrint(x)
+x = re.findall("#?[A-F0-9]{6}", "FF6600")
+checkForMatchAndPrint(x)
+
+# but 3 characters is a valid hex code
+# we can use the | character to indicate 'or' and group them with parenthesis
+x = re.findall("#?([A-F0-9]{6}|[A-F0-9]{3})", "#FF6")
+checkForMatchAndPrint(x)
+
+# for case incensitive stuff you can just add the lower case ones in
+x = re.findall("#([A-Fa-f0-9]{6}|[A-Fa-f0-9])", "#aabb44")
+checkForMatchAndPrint(x)
