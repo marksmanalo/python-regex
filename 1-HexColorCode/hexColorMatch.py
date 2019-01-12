28c910a858499b29a443ac5453b04d1598a8d8eb
Mark Manalo <mmanalo@dealersocket.com>
2019-01-11 17:38:15 -0800

    added more examples

--- 1-HexColorCode/hexColorMatch.py
+++ 1-HexColorCode/hexColorMatch.py
@@ -6,7 +6,7 @@ txt = "#FF6600"
 def checkForMatchAndPrint(regexResult):
   if (regexResult):
     print("We have a match")
-    print(regexResult)
+    print(regexResult.group(0))
   else:
     print("No match")
 
@@ -39,9 +39,24 @@ x = re.match("#?([A-F0-9]{6}|[A-F0-9]{3})", "#FF6")
 checkForMatchAndPrint(x)
 
 # for case incensitive stuff you can just add the lower case ones in
-x = re.match("#?([A-Fa-f0-9]{6}|[A-Fa-f0-9])", "#aabb44")
+x = re.match("#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})", "#aabb44")
 checkForMatchAndPrint(x)
 
 # you can also pass in the IGNORECASE flag
-x = re.match("#?([A-F0-9]{6}|[A-F0-9])", "#aabb44", re.IGNORECASE)
+x = re.match("#?([A-F0-9]{6}|[A-F0-9]{3})", "#aabb44", re.IGNORECASE)
 checkForMatchAndPrint(x)
+
+# re.search will search the entire string until it finds a match
+x = re.search("#?([A-F0-9]{6}|[A-F0-9]{3})", "test # ab122334", re.IGNORECASE)
+checkForMatchAndPrint(x)
+# we can anchor it to search in only specific areas
+# ^: anchors the regex to the beginning of the string
+# $: anchors the regex to the end of the string
+x = re.search("^#?([A-F0-9]{6}|[A-F0-9]{3})$", "test # ab122334", re.IGNORECASE)
+checkForMatchAndPrint(x)
+
+# we can match whitespace with \s
+x = re.search("^\s*#?([A-F0-9]{6}|[A-F0-9]{3})\s*$", "  #AF1245   ", re.IGNORECASE)
+checkForMatchAndPrint(x)
+
+
