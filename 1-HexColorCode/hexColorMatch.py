1c0cd545b0b07ad20cadaa44c07f137c6024cec1
Mark Manalo <mmanalo@dealersocket.com>
2019-01-11 16:36:48 -0800

    added case incensitive flag

--- 1-HexColorCode/hexColorMatch.py
+++ 1-HexColorCode/hexColorMatch.py
@@ -15,29 +15,33 @@ def checkForMatchAndPrint(regexResult):
 # ? - match zero or one time
 # * - match zero or more times
 # + - match one or more times
-x = re.findall("#[ABCDEF0123456789]+", txt)
+x = re.match("#[ABCDEF0123456789]+", txt)
 checkForMatchAndPrint(x)
 
 # for sequential characters we can group them with the '-' character
-x = re.findall("#[A-F0-9]+", txt)
+x = re.match("#[A-F0-9]+", txt)
 checkForMatchAndPrint(x)
 
 # adding a question mark after the # makes it optional (match zero or one time)
 # but this means it will match non hex codes like FF6
-x = re.findall("#?[A-F0-9]+", "FF6")
+x = re.match("#?[A-F0-9]+", "FF6")
 checkForMatchAndPrint(x)
 
 # curly braces can add specificity, here we will match exactly 6 characters
-x = re.findall("#?[A-F0-9]{6}", "FF66")
+x = re.match("#?[A-F0-9]{6}", "FF66")
 checkForMatchAndPrint(x)
-x = re.findall("#?[A-F0-9]{6}", "FF6600")
+x = re.match("#?[A-F0-9]{6}", "FF6600")
 checkForMatchAndPrint(x)
 
 # but 3 characters is a valid hex code
 # we can use the | character to indicate 'or' and group them with parenthesis
-x = re.findall("#?([A-F0-9]{6}|[A-F0-9]{3})", "#FF6")
+x = re.match("#?([A-F0-9]{6}|[A-F0-9]{3})", "#FF6")
 checkForMatchAndPrint(x)
 
 # for case incensitive stuff you can just add the lower case ones in
-x = re.findall("#([A-Fa-f0-9]{6}|[A-Fa-f0-9])", "#aabb44")
+x = re.match("#?([A-Fa-f0-9]{6}|[A-Fa-f0-9])", "#aabb44")
+checkForMatchAndPrint(x)
+
+# you can also pass in the IGNORECASE flag
+x = re.match("#?([A-F0-9]{6}|[A-F0-9])", "#aabb44", re.IGNORECASE)
 checkForMatchAndPrint(x)
